2c09fee84d36ddb591b6671969ed9ecc72a4112c
Isaline <isalineherve@gmail.com>
2025-01-30 15:20:36 +0100

    changement onglet + début page 1

--- app.R
+++ app.R
@@ -25,14 +25,25 @@ str(data)
 #---------- 3. UI ----------
 
 ui <- fluidPage(
-  theme = bslib::bs_theme(bootswatch = "minty"),
-  navlistPanel(
+  theme = bslib::bs_theme(bootswatch = "minty"),tabsetPanel(
     id = "onglet",
+    tabPanel("Recherche selon caractéristiques",
+             sidebarLayout(
+               sidebarPanel(
+                 textInput("ing1", "Ingrédient 1"),
+                 textInput("ing2", "Ingrédient 2"),
+                 textInput("ing3", "Ingrédient 3"),
+                 actionButton("search", "Rechercher")
+               ),
+               
+               mainPanel(
+                 tableOutput("recette_table")
+               )
+             )),
     tabPanel("Recherche par carte"),
-    tabPanel("Recherche selon caractéristiques"),
     tabPanel("Recherche"),
     tabPanel("Information",
-      "Cette application a été créée dans le cadre du cours Dataviz : RShiny du Master 1 ECAP.\n Pour plus d'information, contactez "
+             "Cette application a été créée dans le cadre du cours Dataviz : RShiny du Master 1 ECAP.\n Pour plus d'information, contactez "
     )
   )
 )
@@ -44,8 +55,24 @@ ui <- fluidPage(
 #---------- 4. SERVEUR ----------
 
 server <- function(input, output, session){
-  
+  observeEvent(input$search, {
+    output$recette_table <- renderTable({
+      ingredients <- c(input$ing1, input$ing2, input$ing3) |> 
+        tolower() |> 
+        trimws()
+      
+      ingredients <- ingredients[ingredients != ""]
+      
+      if (length(ingredients) == 0) return(NULL)
+      
+      recettes_filtrees <- recette |>
+        filter(sapply(tolower(ingr_name), function(ing) any(sapply(ingredients, grepl, ing, ignore.case = TRUE))))
+      
+      recettes_filtrees[, c("name", "description")]
+    })
+  })
 }
+  
 
 
 
